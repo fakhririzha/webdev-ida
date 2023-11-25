e449eb159e5395691f508f58767194de9b24a3d6
Fakhri Rizha Ananda <fakhri.rizha@sirclo.com>
2023-11-25 11:57:29 +0700

    Display Card

--- core/modules/homepage/index.js
+++ core/modules/homepage/index.js
@@ -20,19 +20,26 @@ const Homepage = () => {
     }, [data]);
 
     return (
-        <div className='max-w-screen-2xl mx-auto py-[5rem] px-4 sm:px-6 lg:px-16'>
-            <span className='text-3xl font-bold text-neutral-300'>
-                Selamat datang di website IT Developer Academy 2023
-            </span>
-            <br />
-            <span className='text-3xl font-bold text-neutral-300'>
-                {statusFetch ? `Username: ${data.meals[0].strMeal}` : 'Guest'}
-            </span>
-            <br />
-            <span className='text-3xl font-bold text-neutral-300'>
-                {data ? data.meals[0].strMeal : 'Loading...'}
-            </span>
-        </div>
+        <>
+            {data && 
+                <div className='max-w-screen-2xl mx-auto py-[5rem] px-4 sm:px-6 lg:px-16'>
+                    <div className="shadow-2xl rounded-lg w-[400px] bg-slate-800">
+                        <div className="px-8 pt-4 pb-1 font-bold text-white">{data.meals[0].strMeal}</div>
+                        <div className="px-8 pb-2 font-normal text-slate-200">{data.meals[0].strCategory}</div>
+                        <img src={data.meals[0].strMealThumb} alt={data.meals[0].strMeal} className="w-full h-56 object-cover" />
+                        <div className="px-8 pt-6 pb-8 font-normal text-white text-justify">
+                            Instructions:
+                            <br />
+                            <br />
+                            {data.meals[0].strInstructions}
+                        </div>
+                        <div className="border-t-2 border-t-slate-50 px-8 pt-6 pb-8">
+                            <a href={data.meals[0].strYoutube} className="hover:text-yellow-200 text-slate-200">Click Me</a>
+                        </div>
+                    </div>
+                </div>
+            }
+        </>
     );
 };
 
